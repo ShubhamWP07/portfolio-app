8d8aa6fd6e48787054db12bbeed58fa0fe8a0bd9
ShubhamWP07 <shubham1900384@gmail.com>
2023-08-01 01:54:18 +0530

    Header and home coompleted

---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/0ye__its__shubh/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/shubhamsprofile/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/ShubhamWP07"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
